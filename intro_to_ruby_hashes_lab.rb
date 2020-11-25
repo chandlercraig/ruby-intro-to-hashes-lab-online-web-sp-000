c602f85a0c2c92ae9c734357b6d019475a1f8aaf
Chandler Craig <hellochandlercraig@gmail.com>
2020-11-25 17:40:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,14 @@ end
 
 def monopoly_with_third_tier
   monopoly = {
-    railroads: {pieces: 4, names: {}, rent_in_dollars: {}}
+    railroads: {
+      pieces: 4,
+      names: {},
+      rent_in_dollars:
+        {one_piece_owned: 25,
+         two_pieces_owned:
+      }
+    }
 }
 end
 
